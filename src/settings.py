3a6cae4477711f95712598bb39a579f16c9b83f2
Herculino Trotta <netotrotta@gmail.com>
2023-12-25 22:41:22 -0300

    debug

--- src/settings.py
+++ src/settings.py
@@ -30,8 +30,6 @@ BUTTONS = json.loads(
         '[["😄","Very Happy","#ff6961"],["🙂","Happy","#ffb347"],["😐","Neutral","#fdfd96"],["🙁","Sad","#48d148"],["😞","Very Sad","#779ecb"]]',
     )
 )
-if isinstance(BUTTONS, str):
-    BUTTONS = json.loads(BUTTONS)
 
 print(BUTTONS, type(BUTTONS))
 
